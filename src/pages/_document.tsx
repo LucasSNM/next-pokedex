a1fa9e3a6c0d12ab21985a44ce14d1aac552aa87
Lucas Moreira <lucas.moreira@aptiv.com>
2023-07-28 07:47:42 -0300

    initial

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,17 @@
+import { getCssText } from '@/styles'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel='preconnect' href='https://font.googleapis.com' />
+        <link rel='preconnect' href='https://font.gstatic.com' crossOrigin="anonymous"/>
+        <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap' />
+
+        <style id="stitches" dangerouslySetInnerHTML={ {__html: getCssText()} } />
+
+      </Head>
       <body>
         <Main />
         <NextScript />
